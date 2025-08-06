92439bc79e03fb8a1606b8b199272c739b0c9c25
Reid Poole <poole.reid@gmail.com>
2025-08-05 20:29:14 -0600

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function About() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-4">
+      <h1 className="text-3xl font-bold">About Dr. Murray</h1>
+      <Link href="/" className="mt-4 text-blue-500 underline">
+        ← Back Home
+      </Link>
+    </main>
+  );
+}
